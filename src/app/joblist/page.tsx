2e21e3afaa8bbbc5e60050f7545ece9e3d04f01b
Eurico Moutinho <108530777+eurico-moutinho@users.noreply.github.com>
2023-10-10 10:57:30 +0100

    front

---
+++ src/app/joblist/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '../../styles/globals.module.sass'
+import Template from './pageTemplate'
+
+const Joblist = () => {
+    return (
+        <Template />
+    )
+}
+
+export default Joblist
---
+++ src/app/joblist/pageTemplate.js
@@ -0,0 +1,11 @@
+import styles from '../../styles/globals.module.sass'
+
+function Template() {
+    return (
+        <div className={styles.joblist}>
+            Joblist
+        </div>
+    )
+}
+
+export default Template
